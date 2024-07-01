ba67b493880f15ced535fb92d4dbf7f58cc7abca
lebomtomboti <lebomtomboti17@gmail.com>
2024-07-01 10:31:23 +0200

    added

--- index.js
+++ index.js
@@ -1,11 +1,64 @@
-//MinutesToSeconds//
-let input = document.querySelector("input");
-let button = document.querySelector("button");
-let span = document.querySelector("span");
+let lastEdited = "Minutes";  
 
-button.addEventListener("click",MinutesToSeconds)
-function MinutesToSeconds (){
-    span.innerHTML= input.value * 60
+document.getElementById("submit").onclick = convert;
+document.getElementById("reset").onclick = reset;
+
+
+document.getElementById("Minutes").onchange = function() {
+  lastEdited = "Minutes";
+};
+document.getElementById("Seconds").onchange = function() {
+  lastEdited = "Seconds";
+};
+
+
+function convert(Time) {
+
+ 
+  var Minutes = document.getElementById("Minutes").value;
+  Minutes = parseFloat(Minutes);
+
+  var Seconds = document.getElementById("Seconds").value;
+  Seconds = parseFloat(Seconds);
+
+
+
+  var conversionM;
+  var conversionS;
+
+
+
+  if (lastEdited === "Minutes") {
+    conversionS = (Minutes * 60) 
+   
+    document.getElementById("Seconds").value = Math.round(conversionS);
+  } 
+    
+    else if (lastEdited === "Seconds") {
+    conversionM = (Seconds / 60) 
+   
+   
+    document.getElementById("Minutes").value = Math.round(conversionM);
+
+  } 
+  
+
+
+  
+  document.getElementById("Minutes").innerHTML = conversionM;
+  document.getElementById("Seconds").innerHTML = conversionS;
+ 
+}
+
+
+
+ 
+
+
+function reset() {
+  document.getElementById("Minutes").value = 0;
+  document.getElementById("Seconds").value = 0;
+
 }
 
 
